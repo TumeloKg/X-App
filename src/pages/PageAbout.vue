800bd2d5517ef3897433a5c0664900449a353a0a
YoungGuru <tumelo.kgasa@gmail.com>
2023-10-06 10:37:10 +0200

    First commit of the x-clone

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="">
+    <span>About</span>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageAbout",
+});
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="">
+    <span>Home</span>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHome",
+});
+</script>
